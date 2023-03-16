7a7285c3bb232cadc0cb5acb110860c80a2d1f77
asinible <asinible@gitlab.com>
2023-03-16 16:51:51 +0600

    settings

---
+++ project/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+def create_app() -> FastAPI:
+    app  = FastAPI()
+
+    @app.get("/")
+    async def root():
+        return {'message': 'Hello World'}
+
+    return app
+
